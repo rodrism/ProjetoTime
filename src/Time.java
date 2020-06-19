bb5193d70dbec31f9661af58bcb8bf9992d22c73
rodrism <santiagodemenezes@gmail.com>
2020-06-19 16:30:48 -0300

    Exercicios feitos pelo Isidro

--- src/Time.java
+++ src/Time.java
@@ -1,47 +1,53 @@
 
 public class Time {
 	private int hora;
-	private int minuto;
-	private int segundo;
+	private int minu;
+	private int segu;
 	
-	
-	public Time(int hora, int minuto, int segundo) {
+	public void setTime(int hora, int minu, int segu) {
 		this.hora = hora;
-		this.minuto = minuto;
-		this.segundo = segundo;
-	}
-	
-	public int getHora() {
-		return hora;
-	}
-	public void setHora(int hora) {
-		this.hora = hora;
-	}
-	public int getMinuto() {
-		return minuto;
-	}
-	public void setMinuto(int minuto) {
-		this.minuto = minuto;
-	}
-	public int getSegundo() {
-		return segundo;
-	}
-	public void setSegundo(int segundo) {
-		this.segundo = segundo;
-	}
-	
-	public void setTime(int hora, int minuto, int segundo) {
-		this.hora = hora;
-		this.minuto = minuto;
-		this.segundo = segundo;
+		this.minu = minu;
+		this.segu = segu;
 	}
 	
 	public String exibirHoraUniversal() {
-		String strHora = hora + ":" + minuto +":" + segundo;
-		return strHora;
+		return formataNumero(hora)+":"+formataNumero(minu)+":"+formataNumero(segu);
 	}
 	
-	public void exibirHoraPadrao() {
+	public String exibirHoraPadrao() {
+		String strHora;
+		String sufixo;
+		if (hora == 12) {                   // meio dia
+			strHora = formataNumero(12);
+			sufixo = "PM";
+		}
+		else if (hora == 0) {               // meia noite
+			strHora = formataNumero(12);
+			sufixo = "AM";
+		}
+		else if (hora >=1 && hora <= 11) {  // manha
+			strHora = formataNumero(hora);
+			sufixo = "AM";
+		}
+		else {                              // tarde/noite
+			strHora = formataNumero(hora-12);
+			sufixo = "PM";
+		}
+		return strHora+":"+formataNumero(minu)+":"+formataNumero(segu)+sufixo;
 	}
 	
+	private String formataNumero(int num) {
+		if (num < 10) {
+			return "0"+num;
+		}
+		else {
+			return num+"";
+		}
+	}
+	
+	// vou fazer a versao 2 do formata
+	private String fmtNum(int num) {
+		return (num<10) ? "0"+num : String.valueOf(num);
+	}
+
 }
--- src/TimeTeste.java
+++ src/TimeTeste.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
 
 public class TimeTeste {
-
-	public static void main(String[] args) {
+	public static void main(String args[]) {
+		Scanner teclado = new Scanner(System.in);
+		Time t = new Time();
 		
-		Time t = new Time(10,24,43);
-		
+		int h,m,s;
+		h = teclado.nextInt();
+		m = teclado.nextInt();
+		s = teclado.nextInt();
+		t.setTime(h,m,s);
 		
 		System.out.println(t.exibirHoraUniversal());
+		System.out.println(t.exibirHoraPadrao());
+		teclado.close();
+		
 	}
 
 }
